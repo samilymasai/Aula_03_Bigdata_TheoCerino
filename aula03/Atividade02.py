42e5335ea96754d91b70d2f0e2aa69c19bc63fd6
theo.cerino <theogomescerino@gmail.com>
2026-04-02 21:44:33 -0300

    Atividade de média

---
+++ aula03/Atividade02.py
@@ -0,0 +1,8 @@
+aluno01 = float(input("Nota do aluno 01 é :"))
+aluno02 = float(input("Nota do aluno 02 é :"))
+aluno03 = float(input("Nota do aluno 03 é :"))
+aluno04 = float(input("Nota do aluno 04 é :"))
+
+Med_dos_alunos = ( aluno01 + aluno02 + aluno03 + aluno04 ) / 4
+
+print(f'A média dos quatro alunos do total é { Med_dos_alunos}')
